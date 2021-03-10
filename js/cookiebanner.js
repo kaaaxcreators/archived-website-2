a811042f069e979bdb53b83526b8726c161c8a3d
kaaax0815 <999999bst@gmail.com>
2021-03-10 12:45:16 +0100

    cookiebanner

--- js/cookiebanner.js
+++ js/cookiebanner.js
@@ -1,3 +1,10 @@
+var s = document.body.firstChild;
+var gtm = document.createElement('div');
+gtm.setAttribute("id", "footer-cookie");
+gtm.innerHTML="<span id=\"description\"> Wir nutzen Cookies, um unsere Dienste zu erbringen und zu verbessern. Mit Nutzung dieser Seite akzeptieren Sie Cookies. <a href=\"https://kaaaxcreators.de/datenschutz.html\">Datenschutzerklärung</a> </span> <span id=\"accept\"><a href=\"javascript:void(0)\" target=\"_self\" title=\"Akzeptieren\">Akzeptieren</a></span>"; 
+s.parentNode.insertBefore(gtm, s);
+
+
 var footerCookie = document.querySelector("#footer-cookie");
 var footerCookieAccept = document.querySelector("#accept");
 

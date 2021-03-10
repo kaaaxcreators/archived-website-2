df5aee2b895f81e26520ca4d60affccb7f03b0f7
kaaax0815 <999999bst@gmail.com>
2021-03-10 12:08:09 +0100

    cookiebanner

--- js/cookiebanner.js
+++ js/cookiebanner.js
@@ -1,7 +1,7 @@
 var footerCookie = document.querySelector("#footer-cookie");
 var footerCookieAccept = document.querySelector("#accept");
 
-if (document.cookie.indexOf("dakitecCookieBanner=") == -1) {
+if (document.cookie.indexOf("AcceptCookie=") == -1) {
     footerCookie.style.display = "block";
 }
 
@@ -9,7 +9,7 @@ footerCookieAccept.onclick = function (e) {
     var cookieDate = new Date();
     cookieDate.setTime(new Date().getTime() + 31104000000);
 
-    document.cookie = "dakitecCookieBanner = 1; path=/; secure; expires=" + cookieDate.toUTCString();
+    document.cookie = "AcceptCookie = 1; path=/; secure; expires=" + cookieDate.toUTCString();
 
     footerCookie.style.display = "none";
 };
